bbca565e9d7bb291db9fee3b2fc7e27fb7c40e41
maridoroshuk 1 <m,doroshuk@bk.ru>
2022-01-16 12:52:14 +0300

    now it's working with keyboard

--- app.js
+++ app.js
@@ -6,10 +6,13 @@
 
 
 let wordList = [
-// 'patio', 
-// 'darts',
-// 'piano',
-'horse'
+'patio', 
+'darts',
+'piano',
+'horse',
+'pizza',
+'panic',
+'polar',
 ]
 
 let randomIndex = Math.floor(Math.random() * wordList.length);
@@ -22,69 +25,84 @@ let grid = document.getElementById('grid');
 buildGrid()
 updateGrid()
 
+window.addEventListener('keydown', handleKeyDown);
+
+function handleKeyDown(e) {
+	let letter = e.key.toLowerCase();
+	if(letter === 'enter') {
+		if(currentAttempt.length < 5) {
+			return;
+		}
+		if(!wordList.includes(currentAttempt)) {
+			alert('Not in my thesaurus!');
+			return;
+		}
+		history.push(currentAttempt);
+		currentAttempt = '';
+	} else if (letter === 'backspace') {
+		currentAttempt = currentAttempt.slice(0, currentAttempt.length - 1);
+	} else if(/[a-z]/.test(letter)) {
+		if(currentAttempt.length < 5) {
+			currentAttempt += letter;
+		}
+	}
+	updateGrid()
+}
+
+
 function buildGrid() {
 	for (let i = 0; i < 6; i++) {
 		let row = document.createElement('div');
 		for (let j = 0; j < 5; j++) {
 			let cell = document.createElement('div');
 			cell.className = 'cell';
-			row.appendChild(cell);
 			cell.textContent = '';
+			row.appendChild(cell);
 		}
 	grid.appendChild(row);
 	}
 }
 
-
 function updateGrid() {
-	let row = grid.firstChild;
-	for (let attempt of attempts) {
-		drawAttempt(row, attempt, false);
-		row = row.nextSibiling;
-	}
-	drawAttempt(row, currentAttempt, true);
+  let row = grid.firstChild
+  for (let attempt of history) {
+    drawAttempt(row, attempt, false)
+    row = row.nextSibling
+  }
+  drawAttempt(row, currentAttempt, true)
 }
 
 function drawAttempt(row, attempt, isCurrent) {
 	for(let i = 0; i < 5; i++) {
-		let cell = row.children[i];
-		if (attempt[i] !== undefined) {
-		cell.textContent = attempt[i];
-	} else {
-		cell.innerHTML = '<div style="opacity: 0">X</div>';
-	}
-	if(isCurrent) {
-		cell.style.backgroundColor = '#111'
-	} else {
-		cell.style.backgroundColor = getBgColor(attempt, i);
-	}
-
-	}
-}
-
-function drawCurrentAttempt(row, attempt) {
-	for(let i = 0; i < 5; i++) {
-		let cell = row.children[i];
-		if (attempt[i] !== undefined) {
-		cell.textContent = attempt[i];
-	} else {
-		cell.innerHTML = '<div style="opacity: 0">X</div>';
-	}
-	cell.style.backgroundColor = "#111";
+			let cell = row.children[i];
+			if (attempt[i] !== undefined) {
+			cell.textContent = attempt[i];
+		} else {
+			cell.innerHTML = '<div style="opacity: 0">X</div>';
+		}
+		if(isCurrent) {
+			cell.style.backgroundColor = '#361B33'
+		} else {
+			cell.style.backgroundColor = getBgColor(attempt, i);
+		}
 	}
 }
+
 
 function getBgColor(attempt, i) {
 	let correctLetter = secret[i];
 	let attemptLetter = attempt[i];
 	if (attemptLetter === undefined  || 
 		!secret.indexOf(attemptLetter) === -1) {
-		return '#939598';
+		return '#4A2545';
 	}
 	if(correctLetter === attemptLetter) {
-		return '#538d4e';
+		return '#90AA86';
 	}
-	return '#b59f3b';
+	if(secret.includes(attemptLetter)){
+		return '#BC9C4E'
+	}
+	return '#4A2545';
 	
 }
 
